7c544440a77833c92eb35819ea252f0b5557940b
Fabrício G. M. de Carvalho <prof.fabriciogmc@gmail.com>
2021-11-09 07:54:12 -0300

    Teste que falha

--- src/tests/model_test.py
+++ src/tests/model_test.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 
 class ModelTest(TestCase):
     def test_uppercase(self):
-        self.assertEqual(to_uppercase('abc'), 'ABC')
+        self.assertEqual(to_uppercase('abc'), 'ABC_')
 
 if __name__ == '__main__':
     unittest.main()
